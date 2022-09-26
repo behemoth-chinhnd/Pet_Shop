3ff9d53c1c045a30712ea6e9c711e3af1bbfc264
behemoth-chinhnd <chinhnd@behemoth.vn>
2022-09-26 13:52:36 +0700

    add route login

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    resource :auths, only: [] do
+      collection do
+        post :admin_login
+        post :user_login
+      end
+    end
+  end
 end
