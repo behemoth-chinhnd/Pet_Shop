a9bd09f0b0d576a0883704f89867afe57a80f42d
behemoth-chinhnd <chinhnd@behemoth.vn>
2022-10-07 08:13:33 +0700

    add total items in cart serializer

--- server/app/serializers/orders/cart_show_serializer.rb
+++ server/app/serializers/orders/cart_show_serializer.rb
@@ -6,12 +6,17 @@ module Orders
                :shipping_fee,
                :subtotal,
                :discount,
+               :total_quantity,
                :total_items
 
     has_many :order_items
 
-    def total_items
+    def total_quantity
       object.order_items.sum(:quantity)
+    end
+
+    def total_items
+      object.order_items.count
     end
 
     class OrderItemSerializer < ActiveModel::Serializer
