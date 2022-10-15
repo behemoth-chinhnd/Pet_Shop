6e6f77aae76465cb9baa861c117aa5b68a44cade
behemoth-chinhnd <chinhnd@behemoth.vn>
2022-10-16 00:56:36 +0700

    fix bug add to cart quantity

--- server/app/services/orders/add_products_to_cart_service.rb
+++ server/app/services/orders/add_products_to_cart_service.rb
@@ -12,13 +12,15 @@ module Orders
       product = Product.show.find_by(id: context.product_id)
       context.fail!(message: "Not found product") if product.blank?
 
-      order_item = context.current_order.order_items.find_or_create_by!(product_id: context.product_id)
+      ActiveRecord::Base.transaction do
+        order_item = context.current_order.order_items.find_or_create_by!(product_id: context.product_id)
 
-      context.fail!(message: "Not enough product #{product.name} quantity ") if product.quantity < order_item.quantity
+        context.fail!(message: "Not enough product #{product.name} quantity ") if product.quantity < context.quantity
 
-      order_item.increment(:quantity, context.quantity).save!
+        order_item.increment(:quantity, context.quantity).save!
 
-      context.current_order.update_price!
+        context.current_order.update_price!
+      end
     end
   end
 end
